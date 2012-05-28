ad0561d1f9a3d2b51e2da109de1139dc89c1a694
Milan Hanus <mhanus@kma.zcu.cz>
2012-05-28 12:42:00 +0200

    Removing redundant 'friend class Filter'.

--- hermes2d/include/mesh/mesh.h
+++ hermes2d/include/mesh/mesh.h
@@ -206,7 +206,7 @@ namespace Hermes
       template<typename Scalar> friend class DiscreteProblem;
       template<typename Scalar> friend class Solution;
       template<typename Scalar> friend class NeighborSearch;
-      template<typename Scalar> friend class Filter;
+      //template<typename Scalar> friend class Filter;
       template<typename Scalar> friend class MeshFunction;
       template<typename Scalar> friend class Global;
       template<typename Scalar> friend class RefinementSelectors::Selector;
@@ -554,7 +554,7 @@ namespace Hermes
       template<typename Scalar> friend class Global;
       friend class KellyTypeAdapt<std::complex<double> >;
       template<typename Scalar> friend class Solution;
-      template<typename Scalar> friend class Filter;
+      //template<typename Scalar> friend class Filter;
       template<typename Scalar> friend class MeshFunction;
       friend class RefMap;
       friend class Traverse;
