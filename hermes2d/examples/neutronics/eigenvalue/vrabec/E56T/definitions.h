7f64bd293b0f05af5f534518439872e3de779504
Milan Hanus <mhanus@kma.zcu.cz>
2012-10-11 12:48:34 +0200

    Making the VR-1 E56T example compatible with the new API.

--- hermes2d/examples/neutronics/eigenvalue/vrabec/E56T/definitions.h
+++ hermes2d/examples/neutronics/eigenvalue/vrabec/E56T/definitions.h
@@ -1,5 +1,6 @@
 #include "hermes2d.h"
 using namespace Hermes::Hermes2D; 
+using namespace Hermes::Mixins;
 
 #include "weakforms_neutronics.h"
 using namespace Neutronics::Diffusion;
@@ -11,8 +12,4 @@ class CustomWeakForm : public WeakForms::KeffEigenvalueProblem
                    const Hermes::vector<Solution<double>*>& iterates,
                    const Hermes::vector<std::string>& fission_regions,
                    double init_keff, const Hermes::vector<std::string>& bdy_vacuum);
-};
-
-void report_num_dof(const std::string& msg, const Hermes::vector<Space<double> *> spaces);
-void report_errors(const std::string& msg, const Hermes::vector< double > errors);
-                    
+};                    
