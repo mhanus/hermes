26c5a7bec34675b66f5f37cd42ae3e55eee47db9
Milan Hanus <mhanus@kma.zcu.cz>
2013-02-07 21:22:24 +0100

    Updating 1-group-hexagonal eigenvalue example to the new API.

--- hermes2d/examples/neutronics/eigenvalue/1-group-hexagonal/diffusion/definitions.cpp
+++ hermes2d/examples/neutronics/eigenvalue/1-group-hexagonal/diffusion/definitions.cpp
@@ -2,14 +2,9 @@
 #include "definitions.h"
 
 CustomWeakForm::CustomWeakForm( const MaterialProperties::MaterialPropertyMaps& matprop,
-                                const Hermes::vector<Solution<double>*>& iterates, 
-                                const Hermes::vector<std::string>& fission_regions,
-                                double init_keff, const std::string& bdy_vacuum )
-  : WeakForms::KeffEigenvalueProblem(matprop, iterates, fission_regions, init_keff)
-{
+                                const std::string& bdy_vacuum )
+  : WeakForms::FixedSourceProblem(matprop)
+{  
   for (unsigned int g = 0; g < matprop.get_G(); g++)
-  {
-    add_vector_form_surf(new WeakFormParts::VacuumBoundaryCondition::Residual(bdy_vacuum, g));
     add_matrix_form_surf(new WeakFormParts::VacuumBoundaryCondition::Jacobian(bdy_vacuum, g));    
-  }
 }
