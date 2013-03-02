8a41af2363ceafb1407b1fd571005b116ce4bfe3
Milan Hanus <mhanus@kma.zcu.cz>
2013-03-02 09:22:40 +0100

    Watanabe-Maynard problem #2

---
+++ hermes2d/examples/neutronics/fixed-source/Watanabe_Maynard-2/gen_D.m
@@ -0,0 +1,27 @@
+clear all
+
+pweights
+
+ndof = 165888
+N = length(pw)
+ndof_per_ord = ndof/N
+
+k = 1;
+I = zeros(ndof,1);
+J = zeros(ndof,1);
+V = zeros(ndof,1);
+for i=1:ndof_per_ord
+  for j=1:N
+    I(k) = i;
+    J(k) = (j-1)*ndof_per_ord+i;
+    V(k) = pw(j);
+    k = k+1; 
+  end
+end
+
+D = 8*pi*sparse(I, J, V);
+save '-v7' 'D.mat' D
+
+I = 1/(4*pi)*speye(ndof_per_ord);
+M = repmat(I, N, 1);
+save '-v7' 'M.mat' M
---
+++ hermes2d/examples/neutronics/fixed-source/Watanabe_Maynard-2/problem_data.h
@@ -0,0 +1,64 @@
+#include "hermes2d.h"
+using namespace Hermes::Hermes2D;
+
+#include "weakforms_neutronics.h"
+using namespace Neutronics; 
+
+
+//////  Geometric parameters.  /////////////////////////////////////////////////////////////////
+
+// File with initial mesh specification.
+const std::string mesh_file = "domain.mesh";
+
+//////  Physical parameters.  /////////////////////////////////////////////////////////////////
+
+const Hermes::vector<std::string> materials = HermesMultiArray<std::string>
+  ("I")("II")("III")("IV")("V")("Void");
+
+const MaterialPropertyMap0 src = material_property_map<rank0>
+(
+  "I", 0.000
+)(
+  "II", 0.000
+)(
+  "III", 0.000
+)(
+  "IV", 0.000
+)(
+  "V", 0.160
+)(
+  "Void", 0.000
+);
+
+const MaterialPropertyMap1 St = material_property_map<rank1>
+(
+  "I", row(1.0)
+)(
+  "II", row(2.0)
+)(
+  "III", row(1.0)
+)(
+  "IV", row(2.0)
+)(
+  "V", row(0.1)
+)(
+  "Void", row(0.0)  
+);
+
+const MaterialPropertyMap3 Ssn = material_property_map<rank3>
+(
+  "I", page(matrix(row(0.9)))
+)(
+  "II", page(matrix(row(1.5)))
+)(
+  "III", page(matrix(row(0.9)))
+)(
+  "IV", page(matrix(row(1.2)))
+)(
+  "V", page(matrix(row(0.1)))
+)(
+  "Void", page(matrix(row(0.0)))
+);
+
+Hermes::vector<string> edit_regions = HermesMultiArray<string>
+  ("III")("IV");
