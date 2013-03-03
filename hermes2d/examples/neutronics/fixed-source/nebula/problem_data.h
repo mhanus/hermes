a34a3fe6e1c2db04abaaa84f434527667c0faa25
Milan Hanus <mhanus@kma.zcu.cz>
2013-03-03 13:42:04 +0100

    Slowly converging radiative transfer example.

---
+++ hermes2d/examples/neutronics/fixed-source/nebula/problem_data.h
@@ -0,0 +1,16 @@
+#include "hermes2d.h"
+using namespace Hermes::Hermes2D;
+
+#include "weakforms_neutronics.h"
+using namespace Neutronics; 
+
+
+//////  Geometric parameters.  /////////////////////////////////////////////////////////////////
+
+// File with initial mesh specification.
+const std::string mesh_file = "domain.mesh";
+
+//////  Physical parameters.  /////////////////////////////////////////////////////////////////
+
+const double extinction = 1e2;
+const double thermalization = 1e-4;
