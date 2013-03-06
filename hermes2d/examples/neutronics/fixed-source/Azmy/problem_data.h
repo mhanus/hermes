f554abd7ac2f1bbc3a1ef272c96b287b0d20bde5
Milan Hanus <mhanus@kma.zcu.cz>
2013-03-06 10:09:56 +0100

    Azmy benchmark.

---
+++ hermes2d/examples/neutronics/fixed-source/Azmy/problem_data.h
@@ -0,0 +1,37 @@
+#include "hermes2d.h"
+using namespace Hermes::Hermes2D;
+
+#include "weakforms_neutronics.h"
+using namespace Neutronics; 
+
+
+//////  Geometric parameters.  /////////////////////////////////////////////////////////////////
+
+// File with initial mesh specification.
+const std::string mesh_file = "domain.mesh";
+
+//////  Physical parameters.  /////////////////////////////////////////////////////////////////
+
+const Hermes::vector<std::string> materials = HermesMultiArray<std::string>
+  ("M1")("M2");
+
+const MaterialPropertyMap0 src = material_property_map<rank0>
+(
+  "M1", 1.000
+)(
+  "M2", 0.000
+);
+
+const MaterialPropertyMap1 St = material_property_map<rank1>
+(
+  "M1", row(1.0)
+)(
+  "M2", row(2.0)
+);
+
+const MaterialPropertyMap3 Ssn = material_property_map<rank3>
+(
+  "M1", page(matrix(row(0.5)))
+)(
+  "M2", page(matrix(row(0.1)))
+);
