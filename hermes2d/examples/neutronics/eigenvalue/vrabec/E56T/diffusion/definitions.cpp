43e8b9a149f0561b7b2e96ec795fa7038c6fd61a
Milan Hanus <mhanus@kma.zcu.cz>
2013-05-12 18:30:46 +0200

    Making diffusion and SPN versions of the VR-1 example.

--- hermes2d/examples/neutronics/eigenvalue/vrabec/E56T/diffusion/definitions.cpp
+++ hermes2d/examples/neutronics/eigenvalue/vrabec/E56T/diffusion/definitions.cpp
@@ -5,10 +5,8 @@ CustomWeakForm::CustomWeakForm( const MaterialProperties::MaterialPropertyMaps& 
                                 const Hermes::vector<std::string>& bdy_vacuum )
   : WeakForms::FixedSourceProblem(matprop)
 {
-  /*for (unsigned int g = 0; g < matprop.get_G(); g++)
-  {
-    add_vector_form_surf(new WeakFormParts::VacuumBoundaryCondition::Residual(g, bdy_vacuum));
-    add_matrix_form_surf(new WeakFormParts::VacuumBoundaryCondition::Jacobian(g, bdy_vacuum));    
-  }*/
+  if (!bdy_vacuum.empty())
+    for (unsigned int g = 0; g < matprop.get_G(); g++)
+      add_matrix_form_surf(new WeakFormParts::VacuumBoundaryCondition::Jacobian(bdy_vacuum, g));  
 }
 
---
+++ hermes2d/examples/neutronics/eigenvalue/vrabec/E56T/spn/definitions.h
@@ -0,0 +1,13 @@
+#include "hermes2d.h"
+using namespace Hermes::Hermes2D; 
+using namespace Hermes::Mixins;
+
+#include "weakforms_neutronics.h"
+using namespace Neutronics::SPN;
+
+class CustomWeakForm : public WeakForms::FixedSourceProblem
+{
+  public:
+    CustomWeakForm(const MaterialProperties::MaterialPropertyMaps& matprop, unsigned int N,
+                   const Hermes::vector<std::string>& bdy_vacuum);
+};                    
