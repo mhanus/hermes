89f478f6e8a01ee23d4a0a42d087c36044128e06
Milan Hanus <mhanus@kma.zcu.cz>
2013-06-19 11:40:39 +0200

    Neutronics weak forms - update of iterative solution schemes:  - shifted power iteration, Rayleigh quotient, inexact inverse iteration, etc.  - option to measure error by residual  - removing the obsolete version of power iteration

--- hermes2d/include/weakform_library/weakforms_neutronics.h
+++ hermes2d/include/weakform_library/weakforms_neutronics.h
@@ -12,65 +12,111 @@ namespace Hermes { namespace Hermes2D {
     
   class HERMES_API StationaryPicardSolver : public PicardSolver<double>
   {
+    protected:
+      double initial_residual_norm;
+      bool convergence_by_residual;
+      bool have_rhs;
+      bool dynamic_solver_tolerance;
+      bool jacobian_change_on_algebraic_level;
+      
     public:
       StationaryPicardSolver(DiscreteProblem<double>* dp)
-        : PicardSolver<double>(dp) {};
+        : PicardSolver<double>(dp), 
+          convergence_by_residual(false), have_rhs(false), dynamic_solver_tolerance(false), jacobian_change_on_algebraic_level(false) 
+      {
+        constant_jacobian = true;
+      }
+      
       StationaryPicardSolver(WeakForm<double>* wf, SpaceSharedPtr<double>& space) 
-        : PicardSolver<double>(wf, space) {};
+        : PicardSolver<double>(wf, space), 
+          convergence_by_residual(false), have_rhs(false), dynamic_solver_tolerance(false), jacobian_change_on_algebraic_level(false) 
+      {
+        constant_jacobian = true;
+      }
+      
       StationaryPicardSolver(WeakForm<double>* wf, const Hermes::vector<SpaceSharedPtr<double> >& spaces) 
-        : PicardSolver<double>(wf, spaces) {};
-        
+        : PicardSolver<double>(wf, spaces),
+          convergence_by_residual(false), have_rhs(false), dynamic_solver_tolerance(false), jacobian_change_on_algebraic_level(false)
+      {
+        constant_jacobian = true;
+      }
+      
       virtual void solve(double *coeff_vec = NULL);
+      
+      void measure_convergence_by_residual(bool to_set = true) { convergence_by_residual = to_set; }
+      void use_dynamic_solver_tolerance(bool to_set = true);
+      
+      virtual double calculate_relative_error(double* coeff_vec);
+      
+      virtual double calculate_residual_norm() = 0;
       
       using PicardSolver<double>::solve;  // Re-expose other overloaded 'solve' methods hidden by the above override.
   };
   
   namespace Neutronics
-  {
-    /// \brief Power iteration method for finding the dominant eigenvalue. 
-    ///
-    /// Starts from an initial guess stored in the argument 'solutions' and updates it by the final result after the iteration
-    /// has converged, also updating the global eigenvalue 'k_eff'.
-    ///
-    /// \param[in,out] solution     A set of Solution* pointers to solution components (neutron fluxes in each group). 
-    ///                             Initial guess for the iteration on input, converged result on output.
-    /// \param[in]     tol          Relative difference between two successive eigenvalue approximations that stops the iteration.
-    /// \param[in]    matrix_solver Solver for the resulting matrix problem.
-    ///
-    /// \return  number of iterations needed for convergence within the specified tolerance.
-    ///
-    int keff_eigenvalue_iteration(const Hermes::vector<MeshFunctionSharedPtr<double> >& solutions, 
-                                  Common::WeakForms::KeffEigenvalueProblem* wf, const Hermes::vector<SpaceSharedPtr<double> >& spaces,
-                                  MatrixSolverType matrix_solver = SOLVER_UMFPACK, double tol_keff = 1e-6, double tol_flux = 0,
-                                  bool output_matrix_and_rhs = false, EMatrixDumpFormat output_fmt = DF_HERMES_BIN);
-    
-    //TODO: non-vector version
-      
+  {   
     class HERMES_API KeffEigenvalueIteration : public StationaryPicardSolver
     {
       public:
-        KeffEigenvalueIteration(WeakForm<double>* wf, const Hermes::vector<SpaceSharedPtr<double> >& spaces)
-          : StationaryPicardSolver(wf, spaces), keff(1.0), keff_tol(0.0)
+        enum ShiftStrategies { NO_SHIFT = 0, FIXED_SHIFT, RAYLEIGH_QUOTIENT_SHIFT };
+        
+        KeffEigenvalueIteration(WeakForm<double>* wf, const Hermes::vector<SpaceSharedPtr<double> >& spaces, WeakForm<double>* prod_wf = NULL)
+          : StationaryPicardSolver(wf, spaces), 
+            keff(1.0), keff_tol(0.0), fixed_shift(0.0), rayleigh(false), 
+            shift_strategy(NO_SHIFT), unshifted_jacobian(jacobian), num_unshifted_iterations(0),
+            production_matrix(NULL), production_dp(NULL), production_wf(prod_wf), Ax(NULL), have_Ax(false)
         {};
         
-        KeffEigenvalueIteration(DiscreteProblem<double>* dp)
-          : StationaryPicardSolver(dp), keff(1.0), keff_tol(0.0)
+        KeffEigenvalueIteration(DiscreteProblem<double>* dp, WeakForm<double>* prod_wf = NULL)
+          : StationaryPicardSolver(dp), 
+            keff(1.0), keff_tol(0.0), fixed_shift(0.0), rayleigh(false),
+            shift_strategy(NO_SHIFT), unshifted_jacobian(jacobian), num_unshifted_iterations(0),
+            production_matrix(NULL), production_dp(NULL), production_wf(prod_wf), Ax(NULL), have_Ax(false)
         {};
+        
+        virtual ~KeffEigenvalueIteration();
         
         bool on_initialization();
         bool on_step_begin();
         bool on_step_end();
+        bool on_finish();
         
         double get_keff() const { return keff; }
         
         void set_keff_tol(double tol) { keff_tol = tol; }
+        
+        void set_production_weakform(WeakForm<double> *prod_wf) { production_wf = prod_wf; }
+        void set_spectral_shift_strategy(ShiftStrategies strategy, int num_unshifted_iterations = 0, double fixed_shift = 0.0);
+        void set_fixed_spectral_shift(double fixed_shift);
+        
+        void use_rayleigh_quotient(bool to_set = true);
+        
+        virtual void set_spaces(const Hermes::vector<SpaceSharedPtr<double> >& spaces);
+        
+        virtual void init_solving(double* coeff_vec);
+        virtual double calculate_residual_norm();
         
       private:
         double keff;
         double old_keff;
         double keff_tol;
         
+        ShiftStrategies shift_strategy;
+        double fixed_shift;
+        int num_unshifted_iterations;
+        
+        bool rayleigh;
+        
+        double *Ax;
+        bool have_Ax;
+        
+        SparseMatrix<double>* unshifted_jacobian;
+        SparseMatrix<double>* production_matrix;
+        DiscreteProblem<double>* production_dp;
+        WeakForm<double>* production_wf;
+        
         void update();
+        void set_shift(double shift);
     };
     
     class HERMES_API SourceIteration : public StationaryPicardSolver
@@ -83,6 +129,8 @@ namespace Hermes { namespace Hermes2D {
         SourceIteration(DiscreteProblem<double>* dp)
           : StationaryPicardSolver(dp)
         {};
+        
+        virtual double calculate_residual_norm();
     };
     
   /* Neutronics */
