6ef499ddeac3fbe21e6672caf9c575b30fc2e69b
Milan Hanus <mhanus@kma.zcu.cz>
2013-07-10 16:30:10 +0200

    Adding the new (general) external solution loaders.

--- hermes2d/examples/neutronics/utils.cpp
+++ hermes2d/examples/neutronics/utils.cpp
@@ -163,3 +163,84 @@ void load_solution(const std::string& sln_file,
   delete vis;
 }
 
+void load_solution(const std::string& sln_file, 
+                   const Hermes::vector<SpaceSharedPtr<double> >& spaces, 
+                   VisualizationOptions visualization, bool mode_3D)
+{  
+  int ndof =  Space<double>::get_num_dofs(spaces);
+  
+  std::vector<double> x_ext;
+  x_ext.reserve(ndof);
+  std::ifstream ifs( sln_file.c_str() , std::ifstream::in );
+  read_solution_from_file(ifs, std::back_inserter(x_ext));
+  ifs.close();
+  
+  Hermes::vector<MeshFunctionSharedPtr<double> > sol_ext;
+  for (unsigned int i = 0; i < spaces.size(); i++) 
+    sol_ext.push_back(new Solution<double>()); 
+  Solution<double>::vector_to_solutions(x_ext.data(), spaces, sol_ext);
+  
+  if (visualization & (VTK_SCALAR_VISUALIZATION | VTK_ANGULAR_VISUALIZATION))
+  {
+    Hermes::Hermes2D::Views::Linearizer lin;
+    //Hermes::Hermes2D::Views::Orderizer ord;
+    
+    for (unsigned int i = 0; i < spaces.size(); i++)
+    {
+      lin.save_solution_vtk(sol_ext[i], std::string("sln_"+tostr(i)+".vtk").c_str(), std::string("Solution "+tostr(i)).c_str(),
+                            mode_3D, 1, Hermes::Hermes2D::Views::HERMES_EPS_VERYHIGH);
+      //ord.save_mesh_vtk(spaces[i], std::string("mesh_"+tostr(i)+".vtk").c_str());
+      //ord.save_orders_vtk(spaces[i], std::string("ord_"+tostr(i)+".vtk").c_str());
+    }
+  }
+  if (visualization & (HERMES_SCALAR_VISUALIZATION | HERMES_ANGULAR_VISUALIZATION))
+  {
+    Views::ScalarView** views = new Views::ScalarView* [spaces.size()];
+    
+    for (unsigned int i = 0; i < spaces.size(); i++)
+    {
+      views[i] = new Views::ScalarView("Solution", new Views::WinGeom((i*600)%1800, 600*((i*600)/1801), 600, 600));
+      views[i]->set_3d_mode(mode_3D);
+      views[i]->show(sol_ext[i], Hermes::Hermes2D::Views::HERMES_EPS_VERYHIGH);
+    }
+    
+    Views::View::wait();
+    
+    for (unsigned int i = 0; i < spaces.size(); i++)
+      delete views[i];
+    delete [] views;
+  }
+}
+
+void load_solution(const std::string& sln_file, 
+                   const SpaceSharedPtr<double>& space, 
+                   VisualizationOptions visualization, bool mode_3D)
+{  
+  int ndof =  Space<double>::get_num_dofs(space);
+  
+  std::vector<double> x_ext;
+  x_ext.reserve(ndof);
+  std::ifstream ifs( sln_file.c_str() , std::ifstream::in );
+  read_solution_from_file(ifs, std::back_inserter(x_ext));
+  ifs.close();
+  
+  MeshFunctionSharedPtr<double> sol_ext(new Solution<double>()); 
+  Solution<double>::vector_to_solution(x_ext.data(), space, sol_ext);
+  
+  if (visualization & (VTK_SCALAR_VISUALIZATION | VTK_ANGULAR_VISUALIZATION))
+  {
+    Hermes::Hermes2D::Views::Linearizer lin;
+    lin.save_solution_vtk(sol_ext, "sln.vtk", "Solution", mode_3D, 1, Hermes::Hermes2D::Views::HERMES_EPS_VERYHIGH);
+    
+    //Hermes::Hermes2D::Views::Orderizer ord;
+    //ord.save_mesh_vtk(space, "mesh.vtk");
+    //ord.save_orders_vtk(space, "ord.vtk");
+  }
+  if (visualization & (HERMES_SCALAR_VISUALIZATION | HERMES_ANGULAR_VISUALIZATION))
+  {
+    Views::ScalarView view("Solution", new Views::WinGeom(0, 0, 600, 600));
+    view.set_3d_mode(mode_3D);
+    view.show(sol_ext, Hermes::Hermes2D::Views::HERMES_EPS_VERYHIGH);
+    view.wait_for_close();
+  }
+}
